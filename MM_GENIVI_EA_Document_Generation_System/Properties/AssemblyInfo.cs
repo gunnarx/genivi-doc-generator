5025c87af23de5400ab0cf51c58f8f4cad6564fa
Guido Pennella <guido.pennella@gmail.com>
2014-06-03 14:11:15 +0200

    Update licence as per GENIVI request

--- MM_GENIVI_EA_Document_Generation_System/Properties/AssemblyInfo.cs
+++ MM_GENIVI_EA_Document_Generation_System/Properties/AssemblyInfo.cs
@@ -1,16 +1,14 @@
-﻿/* ***** BEGIN LICENSE BLOCK *****
+﻿/* SPDX license identifier: MPL-2.0
  * Author: Guido Pennella
- * Copyright ©2013, Magneti Marelli S.p.A. All rights reserved
+ * Copyright ©2014, Magneti Marelli S.p.A. All rights reserved
  * Version: MPL 2.0
+ * 
+ * This file is part of Magneti Marelli Genivi Document Generator
  *
  * The contents of this file are subject to the Mozilla Public License Version
- * 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the License
+ * 2.0. If a copy of the MPL was not distributed with this file, you can obtain one at http://mozilla.org/MPL/2.0/. 
+ * 
+ * For further information see http://www.genivi.org/. 
  *
  * ***** END LICENSE BLOCK *****/
 using System.Reflection;
--- eadgsPlugin_extractUseCase/Properties/AssemblyInfo.cs
+++ eadgsPlugin_extractUseCase/Properties/AssemblyInfo.cs
@@ -1,4 +1,17 @@
-﻿using System.Reflection;
+﻿/* SPDX license identifier: MPL-2.0
+ * Author: Guido Pennella
+ * Copyright ©2014, Magneti Marelli S.p.A. All rights reserved
+ * Version: MPL 2.0
+ * 
+ * This file is part of Magneti Marelli Genivi Document Generator
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 2.0. If a copy of the MPL was not distributed with this file, you can obtain one at http://mozilla.org/MPL/2.0/. 
+ * 
+ * For further information see http://www.genivi.org/. 
+ *
+ * ***** END LICENSE BLOCK *****/
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
